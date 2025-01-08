57308dd30f1bfb4990b23e2d6ca7592bc7cca27f
ankit-jds <84713907+ankit-jds@users.noreply.github.com>
2025-01-09 00:49:06 +0530

    ADD: snapshot schedule job, apscheduler setup

--- collab/settings.py
+++ collab/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -31,6 +32,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -125,3 +127,5 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
--- collab/urls.py
+++ collab/urls.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/c/', include("collaboration.urls")),
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+from datetime import datetime, timedelta
+from typing import Callable
 
+from collaboration.jobs.snapshot_job import snapshot_job
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/c/", include("collaboration.urls")),
 ]
+scheduler = BackgroundScheduler()
+
+
+def schedule_next_execution(
+    function: Callable[[], None], interval_in_secs: int = 20
+) -> None:
+    now = datetime.now()
+    next_run_time = now + timedelta(seconds=interval_in_secs)
+
+    print(f"Next job will run at {next_run_time}")
+
+    def wrapper_function():
+        function()
+        schedule_next_execution(function=function, interval_in_secs=interval_in_secs)
+
+    scheduler.add_job(wrapper_function, "date", run_date=next_run_time)
+
+
+schedule_next_execution(function=snapshot_job)
+
+try:
+    scheduler.start()
+except (KeyboardInterrupt, SystemExit):
+    scheduler.shutdown()
+    print("Scheduler stopped.")
